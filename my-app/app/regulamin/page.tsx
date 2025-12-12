b6b8fe08bcee0b586d5b070d7282feefb83047a5
Martyna <m.kolaska04@gmail.com>
2025-12-12 09:13:57 +0100

    fix

---
+++ my-app/app/regulamin/page.tsx
@@ -0,0 +1,8 @@
+export default function Regulamin() {
+    return (
+        <div>
+            <h1>Regulamin</h1>
+            {/* Add the content of the regulations here */}
+        </div>
+    );
+}

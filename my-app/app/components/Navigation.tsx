c16af2dd53152665f062df1efcd3f1a29d439951
Martyna <m.kolaska04@gmail.com>
2025-12-12 09:05:42 +0100

    add variant 2

--- my-app/app/components/Navigation.tsx
+++ my-app/app/components/Navigation.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { useState } from "react";
+import {useRouter} from "next/navigation";
+import Link from "next/dist/client/link";
 
 export default function Navigation() {
+    const router = useRouter();
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     const toggleDrawer = () => {
         setIsDrawerOpen(!isDrawerOpen);
@@ -11,12 +14,13 @@ export default function Navigation() {
     return (
         <nav className="w-full p-4 bg-background/50 shadow-md fixed top-0 left-0 z-50">
             <div className=" mx-auto flex justify-between items-center">
-                <div className="text-white font-bold text-2xl font-sans">Idea2Impact</div>
+                <div className="text-white font-bold text-2xl font-sans">Idea2Impact         <a onClick={() => router.push('/var2')} className="text-white mx-2 text-sm hover:text-secondary font-code">wariant drugi HomePage</a></div>       
                 <div className="hidden md:flex">
-                    <a href="#" className="text-white mx-2 text-xl hover:text-secondary font-code">O Wydarzeniu</a>
-                    <a href="#" className="text-white mx-2 text-xl hover:text-secondary font-code">Organizatorzy</a>
-                    {/* <a href="#" className="text-white mx-2 text-xl hover:text-secondary font-code">Zapisy</a> */}
-                    <a href="#" className="text-white mx-2 text-xl hover:text-secondary font-code">Regulamin</a>
+                    <a onClick={() => router.push('/')}  className="text-white mx-2 text-xl hover:text-secondary font-code">O Wydarzeniu</a>
+                    {/* <a href="#" className="text-white mx-2 text-xl hover:text-secondary font-code">Organizatorzy</a> */}
+            
+                    <Link href="#" className="text-white mx-2 text-xl hover:text-secondary font-code">Zapisy</Link>
+                    <a onClick={() => router.push('/regulamin')} className="text-white mx-2 text-xl hover:text-secondary font-code">Regulamin</a>
                 </div>
                 <div className="md:hidden flex items-center">
 
--- my-app/app/var2/page.tsx
+++ my-app/app/var2/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import LiquidEther from './components/LiquidEther';
-import Orb from './components/Orb';
-import Timer from './components/Timer';
-import Divider from './components/Divider';
+import Orb from '../components/Orb';
+import Timer from '../components/Timer';
+import Divider from '../components/Divider';
+
 
 export default function Home() {
   const [windowHeight, setWindowHeight] = useState<number | string>('100vh');
   const eventDate = new Date('2025-12-31T10:00:00');
+
 
   useEffect(() => {
     const handleResize = () => {
@@ -27,28 +28,18 @@ export default function Home() {
     <div className="flex flex-col min-h-screen items-center justify-center font-sans ">
 
 
-      <div style={{ width: '100%', height: windowHeight || '100vh', position: 'relative', backgroundColor: 'var(--color-background)' }}>
-        <LiquidEther
-          colors={['#5227FF', '#FF9FFC', '#B19EEF']}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={true}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.5}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.3}
-          autoIntensity={3.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={1000}
-          autoRampDuration={0.5}
-          dt={0.014}
-          BFECC={true}
-
-        />
-        <section className="flex flex-col justify-center w-fit absolute bottom-8 left-8 lg:bottom-12 lg:left-12">
+     
+      <div className="w-full flex items-center justify-center">
+        <div style={{ width: '60%', height: windowHeight || '100vh', position: 'relative' }}>
+          <Orb
+            hoverIntensity={0.5}
+            rotateOnHover={true}
+            hue={250}
+            forceHoverState={false}
+          />
+        </div>
+         
+          <section className="flex flex-col justify-center w-fit absolute bottom-8 left-8 lg:bottom-12 lg:left-12">
           <h1 className="text-5xl font-grotesk font-bold">Idea2Impact <span className="text-secondary animate-blink" id="cursor">|</span></h1>
           <p className="text-2xl font-grotesk">Hackathon 2025</p>
         </section>
@@ -60,26 +51,26 @@ export default function Home() {
           <Timer eventDate={eventDate} />
           <button className="mt-8 px-6 py-3 bg-white text-primary font-semibold font-inter rounded-lg hover:bg-gray-300 transition w-fit mx-auto text-2xl">Zapisz się!</button>
         </div>
-
       </div>
+
       <main className=" w-full mt-16 mb-8 space-y-8">
         <Divider />
-        <h2 className='text-3xl font-semibold text-center'>&lt;-- O wydarzeniu --&gt;</h2>
-        <section className=" max-w-7xl p-8 flex  gap-8 flex-col justify-between items-between w-full min-h-[600px] w-full lg:w-3/5 mx-auto" >
-          <div className='max-w-2xl self-start text-lg'>
+        <h2 className='text-3xl text-center'>&lt;-- O wydarzeniu --&gt;</h2>
+        <section className="p-8 flex  gap-8 flex-col justify-between items-between w-full min-h-[600px] w-full lg:w-3/5 mx-auto" >
+          <div className='max-w-xl self-start text-lg'>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo culpa, sunt corrupti voluptatum optio totam non dicta deserunt debitis asperiores sit? Sequi earum doloremque vitae eum obcaecati hic debitis distinctio.
             Iure animi illo veniam quod autem tenetur ut vel eum? Molestiae hic, nemo error similique perferendis veritatis alias ad eaque porro est assumenda expedita eius! Autem vel expedita modi officia.
             Atque, inventore, eaque est animi iusto corporis non tempora veritatis obcaecati <span className='text-secondary'>24h </span>nihil minus culpa ex quam aperiam fugit in sunt voluptas architecto. Impedit ipsam porro asperiores quidem ratione assumenda reiciendis.
             Excepturi alias et nostrum quibusdam laborum, optio nam eveniet harum numquam tempore veritatis mollitia dolorum vel quisquam eius debitis commodi a facere atque asperiores nobis. Enim fugit animi sed quas.
           </div>
-          <div className='max-w-2xl self-end text-lg'>
+          <div className='max-w-xl self-end text-lg'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni rem porro officia exercitationem delectus nemo ab illo, assumenda iste molestiae blanditiis minima pariatur veritatis dolorum voluptates odit corrupti quos voluptatum.
             Iste reiciendis saepe quasi corporis atque sapiente quia vitae soluta a natus asperiores maxime modi mollitia recusandae ratione explicabo laborum vel facilis molestiae sequi porro minus, at fugit. Excepturi, ea.
             Dicta recusandae officiis officia distinctio vero tempora praesentium mollitia, perferendis assumenda, <span className='text-secondary'>voluptate </span> rem rerum a nihil, nulla accusantium. Consequatur itaque incidunt eveniet atque fugit, facere reiciendis deleniti quam alias libero?
           </div>
         </section>
         <Divider />
-        <h2 className='text-3xl font-semibold  text-center'>&lt;-- Lokalizacja wydarzenia --&gt;</h2>
+        <h2 className='text-3xl text-center'>&lt;-- Lokalizacja wydarzenia --&gt;</h2>
         <section className="w-full lg:w-3/5 mx-auto p-8">
           <div className="w-full h-[450px] rounded-lg overflow-hidden shadow-lg">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1022.348695312378!2d18.572401673396413!3d54.396282890011996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fd752655cecc63%3A0x5dc2caea70bb6d84!2sInstytut%20Informatyki%20UG!5e0!3m2!1spl!2spl!4v1765209333072!5m2!1spl!2spl"
@@ -98,7 +89,7 @@ export default function Home() {
 
         </section>
         <Divider />
-        <h2 className='text-3xl font-semibold  text-center'>&lt;-- Organizatorzy --&gt;</h2>
+        <h2 className='text-3xl text-center'>&lt;-- Organizatorzy --&gt;</h2>
         <section className="w-full lg:w-3/5 mx-auto">
 
         </section>

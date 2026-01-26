d65b11ca57f09c4d655d9058ca009f523b3d2f25
Martyna <m.kolaska04@gmail.com>
2026-01-26 10:16:04 +0100

    loga inicjatorow

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -18,6 +18,7 @@ import RecordVoiceOverIcon from "@mui/icons-material/RecordVoiceOver";
 import Link from "next/link";
 import Modal from "./components/Modal";
 import Image from "next/image";
+import LogoLoop from "./components/Logo";
 
 export default function Home() {
   const [windowHeight, setWindowHeight] = useState<number | string>("100vh");
@@ -35,6 +36,14 @@ export default function Home() {
   const now = new Date();
   const isRegistrationOpen =
     now >= registrationStartDate && now <= registrationEndDate;
+
+  const ChallLogos = [
+  { src: "Infiro_logo.png", alt: "Fundacja Infiro", href: "https://infiro.pl" },
+  { src: "logo_wip.png", alt: "Koło naukowe WIP" },
+  { src: "pd_logo.png", alt: "Stowarzyszenie Pomorski Dom", href: "https://pomorskidom.com" },
+  { src: "dns_logo.png", alt: "Stowarzyszenie Dom na Skraju", href: "https://domnaskraju.pl" },
+  { src: "lg_logo.png", alt: "Fundacja L’Arche Gdynia", href: "http://larche.org.pl/gdynia/http://larche.org.pl/gdynia/" },
+];
 
   useEffect(() => {
     const handleResize = () => {
@@ -281,6 +290,18 @@ export default function Home() {
                 <li className="text-justify">Fundacja L’Arche Gdynia</li>
                 <li className="text-justify">Koło naukowe WIP </li>
               </ul>
+               <LogoLoop
+        logos={ChallLogos}
+        speed={80}
+        direction="left"
+        logoHeight={60}
+        gap={60}
+        hoverSpeed={0}
+        scaleOnHover
+        fadeOut
+        fadeOutColor="#151515"
+        ariaLabel="Inicjatorzy wyzwań"
+      />
             </div>
           </AnimatedContent>
           <div className="flex justify-center items-center text-white mt-4 text-center mx-auto ">

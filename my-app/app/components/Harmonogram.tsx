9fe46e30ea1bd7ffc56a57945dba9b605fae77fb
Martyna <m.kolaska04@gmail.com>
2026-03-03 19:01:44 +0100

    update harmonogram

--- my-app/app/components/Harmonogram.tsx
+++ my-app/app/components/Harmonogram.tsx
@@ -5,13 +5,37 @@ import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 export default function Harcodegram() {
   return (
     <>
-      <h2 className="my-4 text-3xl lg:text-4xl font-inter font-semibold  text-center text-primary max-w-5xl mx-auto px-4">
+      <h2 className="my-8 text-3xl lg:text-4xl font-inter font-semibold  text-center text-primary max-w-5xl mx-auto px-4">
         Harmonogram
       </h2>
       <section className="w-full max-w-5xl px-4 md:px-8 lg:px-12 mx-auto overflow-hidden">
         <AnimatedContent
           distance={50}
           direction="horizontal"
+          reverse={true}
+          duration={1.2}
+          ease="power3.out"
+          initialOpacity={0.2}
+          animateOpacity
+          scale={1.0}
+          threshold={0.2}
+          delay={0.3}
+        >
+          <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
+            <h3 className="text-secondary text-lg lg:text-xl">
+              Etap I: Sesja startowa; 18:00-20:30
+            </h3>
+            <p>
+              Podczas sesji startowej przedstawimy zasady i cele wydarzenia. W
+              tej części odbędzie się również prezentacja problemów oraz wybór
+              tematów przez uczestników. Następnie zaplanowane są warsztaty
+              wprowadzające.
+            </p>
+          </div>
+        </AnimatedContent>
+        <AnimatedContent
+          distance={50}
+          direction="horizontal"
           reverse={false}
           duration={1.2}
           ease="power3.out"
@@ -22,11 +46,17 @@ export default function Harcodegram() {
           delay={0.3}
         >
           <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
-            <h3 className="text-secondary text-lg lg:text-xl">Faza Wstępna</h3>
+            <h3 className="text-secondary text-lg lg:text-xl">
+              Etap II: Faza Rozwoju; 20:30-12:15
+            </h3>
             <p>
-              Uczestnicy otrzymają szczegółową dokumentację z zasadami,
-              wytycznymi i terminami przed właściwym wydarzeniem.
+              W czasie tego etapu zespoły pracują nad projektami. Zaplanowano
+              przerwy na posiłek w następujących godzinach:
             </p>
+            <ul className="list-disc list-inside ml-4">
+              <li>ok. 22 – 7 marca,</li>
+              <li>8:00 – 8 marca</li>
+            </ul>
           </div>
         </AnimatedContent>
         <AnimatedContent
@@ -43,25 +73,14 @@ export default function Harcodegram() {
         >
           <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
             <h3 className="text-secondary text-lg lg:text-xl">
-              Etap 1: Sesja startowa
+              Etap III: Przygotowanie strategii prezentacji rozwiązania
+              12:15-14:15
             </h3>
             <p>
-              Wyjaśnienie zasad, celów i logistyki. Organizatorzy przedstawią
-              wprowadzenie do wydarzenia oraz ujawnią oficjalne wyzwania lub
-              tematy. <span className="text-primary">Start: Ok. 18:00.</span>
-            </p>
-            <h4 className="text-secondary mt-2">Warsztaty/prezentacje</h4>
-            <p>
-              Na początku przewidziane są warsztaty dotyczące zarządzania
-              projektami, prototypowania, pracy zespołowej, elementów design
-              thinking oraz mindfulness.
-            </p>
-            <h4 className="text-secondary mt-2">Prezentacja Problemów</h4>
-            <p>
-              Prezentacja wyzwań zgłoszonych przez organizacje partnerskie.
-              Przedstawiciele Beneficjentów będą obecni na miejscu, aby omówić
-              szczegóły problemów, a także pozostaną do dyspozycji telefonicznej
-              przez cały czas trwania Hackathonu.
+              W trakcie tego etapu odbędzie się warsztat dotyczący przygotowania
+              prezentacji produktowej – jest to czas, aby przygotować
+              prezentację i przygotować się do wystąpienia.
+              <br />O 13:00 odbędzie się przerwa na lunch.
             </p>
           </div>
         </AnimatedContent>
@@ -79,66 +98,13 @@ export default function Harcodegram() {
         >
           <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
             <h3 className="text-secondary text-lg lg:text-xl">
-              Etap 2: Faza Rozwoju
+              Etap IV: Termin zgłoszeń
             </h3>
             <p>
-              Intensywna praca zespołów nad projektami.
-              Zachęca się zespoły do iteracji pomysłów oraz rozwijania
-              innowacyjnych, ale wykonalnych rozwiązań.
-            </p>
-            <p className="flex items-center mt-2">
-              <AccessAlarmIcon />
-              Przerwy: Przewidziana jest przerwa na pizzę oraz
-              przerwa na śniadanie.
-            </p>
-          </div>
-        </AnimatedContent>
-        <AnimatedContent
-          distance={50}
-          direction="horizontal"
-          reverse={true}
-          duration={1.2}
-          ease="power3.out"
-          initialOpacity={0.2}
-          animateOpacity
-          scale={1.0}
-          threshold={0.2}
-          delay={0.3}
-        >
-          <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
-            <h3 className="text-secondary text-lg lg:text-xl">
-              Etap 3: Sesje Kontrolne
-            </h3>
-            <p>
-              Organizatorzy mogą planować punkty kontrolne w celu oceny
-              postępów, zapewnienia informacji zwrotnych i utrzymania tempa
-              pracy. Są to obowiązkowe aktualizacje postępów.
-            </p>
-          </div>
-        </AnimatedContent>
-        <AnimatedContent
-          distance={50}
-          direction="horizontal"
-          reverse={false}
-          duration={1.2}
-          ease="power3.out"
-          initialOpacity={0.2}
-          animateOpacity
-          scale={1.0}
-          threshold={0.2}
-          delay={0.3}
-        >
-          <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
-            <h3 className="text-secondary text-lg lg:text-xl">
-              Etap 4: Termin składania zgłoszeń
-            </h3>
-            <p>
-              Projekty muszą zostać przesłane przed oficjalnym terminem za
-              pośrednictwem wyznaczonej platformy (np. GitHub lub Google Drive).
-              Zgłoszenie powinno zawierać jasną reprezentację rozwiązania
-              (prototyp, koncepcja, makieta) wraz z krótkim wyjaśnieniem jego
-              celu i wpływu. Dotrzymanie terminu jest wymagane, aby pozostać
-              uprawnionym do oceny.
+              Każdy zespół ma obowiązek przesłać <span className=" font-bold">zgłoszenie projektu do godziny
+              14:15, 8 marca</span>. Szczegóły dotyczące wymaganego produktu zostały
+              przedstawione w sekcji nr 2. Materiały należy przesłać na adres:
+              kontakt@infiro.pl
             </p>
           </div>
         </AnimatedContent>
@@ -157,27 +123,12 @@ export default function Harcodegram() {
         >
           <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
             <h3 className="text-secondary text-lg lg:text-xl">
-              Etap 5: Prezentacje Finałowe
+              Etap V: Prezentacje Finałowe
             </h3>
             <p>
-              Zespoły prezentują swoje projekty Jurorom. Prezentacje powinny
-              trwać od pięciu do dziesięciu minut.
-              {/* <span className="text-primary">
-                {" "}
-                Prezentacja prac: Ok. godz. 13:00.{" "}
-              </span> */}
+              Będą one miały miejsce od 14:30 do 17:00. Czas prezentacji jednego
+              zespołu nie powinien przekroczyć  <span className=" font-bold">3 minut</span>.
             </p>
-            <p className="mt-2 text-tertiary">Wymaganym produktem prac jest:</p>
-            <ul className="list-disc list-inside ml-4">
-              <li>opis produktu i plan wdrożenia opisany np. w word,</li>
-              <li>
-                prototyp (Prototyp może być różny w zależności od natury
-                rozwiązania, np.: pseudokod, kod programistyczny, aplikacja,
-                biznes plan, makieta, plansza, plakat, piosenka, scenka.)
-              </li>
-              <li>prezentacja.</li>
-            </ul>
-            <p></p>
           </div>
         </AnimatedContent>
         <AnimatedContent
@@ -194,11 +145,15 @@ export default function Harcodegram() {
         >
           <div className="p-4 border-2 border-accent rounded-md mb-4 break-words">
             <h3 className="text-secondary text-lg lg:text-xl">
-              Etap 6: Ocena i Ceremonia Wręczenia Nagród
+              Etap VI: Ocena projektów przez Jury oraz ceremonia wręczenia
+              nagród.
             </h3>
             <p>
-              Jurorzy oceniają projekty zgodnie z określonymi kryteriami.
-              {/* <span className="text-primary"> Wyniki, nagrody: Ok. 15:00.</span> */}
+              Będzie to czas podsumowania projektów i celebracji tych
+              najlepszych. W tym czasie odbędzie się również prezentacja
+              Fundacji Inicjowania Rozwoju Up Foundation w której zostaną
+              przedstawione perspektywy rozwijania swoich inicjatyw. Koniec
+              wydarzenia planowany jest na godzinę 18:00.{" "}
             </p>
           </div>
         </AnimatedContent>
